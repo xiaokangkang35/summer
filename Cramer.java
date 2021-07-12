ffd327d9d0230390074545ac48c448c16159a090
康宝 <72679082+xiaokangkang35@users.noreply.github.com>
2021-07-12 21:00:11 +0800

    Add files via upload

---
+++ Cramer.java
@@ -0,0 +1,18 @@
+
+public class Cramer {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		double a = 21.8;
+		int b = 2;
+		int e = 28;
+		int c = 7;
+		int d = 8;
+		int f = 62;
+		double x = (e*d-b*f)/(a*d-b*c);
+		double y = (a*f-e*c)/(a*d-b*c);
+		System.out.println(x);
+		System.out.println(y);
+	}
+
+}
