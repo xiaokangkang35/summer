0a6371b89fb414b8e2f4450ff3704c9e30ba5a9c
康宝 <72679082+xiaokangkang35@users.noreply.github.com>
2021-07-12 20:59:04 +0800

    Add files via upload

---
+++ acceleration.java
@@ -0,0 +1,13 @@
+
+public class acceleration {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		double time = 8.7 / 3600;
+		int speed1 = 0;
+		int speed2 = 100;
+		double acceleration = (speed2 - speed1) / time;
+		System.out.println("acceleration = "+acceleration);
+	}
+
+}
